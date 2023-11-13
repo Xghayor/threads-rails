8bec91b47e225e2b17895b1c1e830ae941e775da
Xghayor <cc.baqir@gmail.com>
2023-11-13 19:29:20 +0000

    fix required changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-resources :users, only: [:index, :show]
-
-resources :posts, only: [:index, :show] 
-end
-
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show]
+    end
+  end
+  
